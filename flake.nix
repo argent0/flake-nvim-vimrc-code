290e0cfe761df18d70bb76d19089da9991c12685
Aner Lucero <4rgento@gmail.com>
2023-04-12 23:30:59 -0300

    Export neovim, plugins, and derivations as a library.

--- flake.nix
+++ flake.nix
@@ -1,50 +1,63 @@
 {
-  description = "A vimrc to configure vim for coding";
+  description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: 
-  let
-    src = ./vimrc;
+  outputs = { self, nixpkgs }:  let
+
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
 
-    # The package output for this flake, which installs the Vim configuration
-    # file into the Nix store at the path /etc/nvim/vimrc
-    packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
-      name = "nvim-vimrc-code";
-      src = ./.;
-      installPhase = ''
-        mkdir -p $out/etc/nvim
-        cp vimrc $out/etc/nvim/vimrc
-      '';
-    };
+    baseVimrc = ''
+" show number
+set number
+" highlight the 80th column
+set colorcolumn=80
+" set the column highlight color to light blue
+highlight ColorColumn ctermbg=lightblue guibg=lightblue
+" don't wrap lines
+set nowrap
+    '';
 
-    # The devShell environment output for this flake, which provides a shell
-    # environment that includes the custom Vim configuration and other
-    # development tools
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = with pkgs; let
-        vimrcPath =
-          self.packages.x86_64-linux.default.outPath + "/etc/nvim/vimrc";
-          local-neovim = neovim.override {
-            configure = {
-              # Additional plugins to be installed
-              packages.myVimPackages = with pkgs.vimPlugins; {
-                start = [
-                  vim-nix
-                  copilot-vim
-                ];
-                opt = [ ];
-              };
-              customRC = builtins.readFile vimrcPath;
+    nvimShell = {
+      version = "1.0.0";
+      neovim = {
+        extraVimrcLines ? "",
+        extraVimPlugins ? [ ],
+        extraNixDerivations ? [ ],
+      }:
+      let
+        vimrc = baseVimrc + "\n" + extraVimrcLines;
+        vimPlugins = {
+          start = ( with pkgs.vimPlugins; [
+            vim-nix
+            copilot-vim
+          ]) ++ extraVimPlugins;
+          opt = [ ];
+        };
+        local-neovim = pkgs.neovim.override {
+          configure = {
+            customRC = vimrc;
+            packages.myVimPackage = {
+              start = vimPlugins.start;
+              opt = vimPlugins.opt;
             };
           };
-      in [
-        self.packages.x86_64-linux.default
-        nodejs
-        local-neovim
-        git
-      ];
+        };
+      in pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs
+          local-neovim
+        ] ++ extraNixDerivations;
+      };
     };
+  in {
+
+    lib = nvimShell;
+
+    packages.x86_64-linux.default = pkgs.buildEnv {
+      name = "nvimShell";
+      paths = [];
+    };
+
+    devShell.x86_64-linux = nvimShell.neovim { };
 
   };
 }
